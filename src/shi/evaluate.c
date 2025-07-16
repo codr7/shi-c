a83c7b196015bb4796aa04a7155b53a9b92139a5
codr7 <codr7@protonmail.com>
2025-07-16 15:49:51 +0200

    Add call op type check.

--- src/shi/evaluate.c
+++ src/shi/evaluate.c
@@ -1,6 +1,9 @@
+#include "shi/error.h"
 #include "shi/evaluate.h"
+#include "shi/libraries/core.h"
 #include "shi/method.h"
 #include "shi/stack.h"
+#include "shi/type.h"
 #include "shi/vm.h"
 
 static uint8_t *call_method_evaluate(struct sh_vm *vm,
@@ -8,8 +11,25 @@ static uint8_t *call_method_evaluate(struct sh_vm *vm,
 				     uint8_t *data) {
   struct sh_call_method *op =
     (void *)sh_align(data, alignof(struct sh_call_method));
+
+  struct sh_method *m = op->target;
+
+  if (stack->items.length < m->arity) {
+    sh_throw("Error in %s: Not enough arguments", sh_sloc_string(&op->sloc));
+  }
+
+  for (int i = 0; i < m->arity; i++) {
+    struct sh_cell *v = sh_vector_get(&stack->items, stack->items.length - i - 1);
+    const struct sh_type *at = m->arguments[i].type;
+
+    if (at != SH_ANY() && !sh_isa(v, at)) {
+      sh_throw("Error in %s: Type mismatch; expected %s, actual %s",
+	       sh_sloc_string(&op->sloc), at->name, v->type->name);
+    }
+  }
+
   size_t pc = sh_pointer_pc(vm, (uint8_t *)op + sizeof(struct sh_call_method));
-  sh_method_call(op->target, &pc, stack,  &op->sloc);
+  sh_method_call(m, &pc, stack,  &op->sloc);  
   return sh_pc_pointer(vm, pc);
 }
 
